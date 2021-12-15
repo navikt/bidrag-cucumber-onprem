244f5e14a4c828922e574f2d82db974ff99071da
LarsOttoHaugen <Norge2355>
2021-12-15 11:46:45 +0100

    Lagt til tester for bidrag-organisasjon

---
+++ src/main/kotlin/no/nav/bidrag/cucumber/onprem/organisasjon/OrganisasjonEgenskaper.kt
@@ -0,0 +1,24 @@
+package no.nav.bidrag.cucumber.onprem.organisasjon
+
+import io.cucumber.java8.No
+import no.nav.bidrag.cucumber.model.CucumberTestRun
+
+class OrganisasjonEgenskaper : No {
+  init {
+    Når("jeg henter informasjon om saksbehandler med ident {string}") { ident: String ->
+      CucumberTestRun.hentRestTjenesteTilTesting().exchangeGet("/saksbehandler/info/$ident")
+    }
+
+    Når("jeg henter enheter for saksbehandler med ident {string}") { ident: String ->
+      CucumberTestRun.hentRestTjenesteTilTesting().exchangeGet("/saksbehandler/enhetsliste/$ident")
+    }
+
+    Når("jeg henter journalfoerende enheter fra arbeidsfordeling") {
+      CucumberTestRun.hentRestTjenesteTilTesting().exchangeGet("/arbeidsfordeling/enhetsliste/journalforende")
+    }
+
+    Når("jeg henter enheter fra arbeidsfordeling for person med ident {string}") { ident: String ->
+      CucumberTestRun.hentRestTjenesteTilTesting().exchangeGet("/arbeidsfordeling/enhetsliste/geografisktilknytning/$ident")
+    }
+  }
+}
